5f025d4526b431e2d1ec5d8d4c647a5ab346ebb0
KOISTUBH KASADIWAR <KASADIWARKAUSTUBH@GMAIL.COM>
2024-03-10 15:12:03 +0800

    Final Commit

--- src/main/java/com/gic/geopuzzle/GeometryPuzzle.java
+++ src/main/java/com/gic/geopuzzle/GeometryPuzzle.java
@@ -6,14 +6,16 @@ import java.util.stream.IntStream;
 public class GeometryPuzzle{
     public static List<List<Integer>> shape = new ArrayList<>();
 
-    public static void createCustomShape(Scanner scanner) {
+    public void createCustomShape(Scanner scanner) {
         System.out.println("Please enter coordinates 1 in x y format");
-        addCoordinate(scanner);
+        addCoordinate(scanner,0,0);
     }
 
-    private static void addCoordinate(Scanner scanner) {
-        Integer x = scanner.nextInt();
-        Integer y = scanner.nextInt();
+    private static void addCoordinate(Scanner scanner,Integer x, Integer y) {
+        if(x==0 && y ==0) {
+             x = scanner.nextInt();
+             y = scanner.nextInt();
+        }
         List<Integer> point = new ArrayList<>();
         point.add(x);
         point.add(y);
@@ -24,72 +26,103 @@ public class GeometryPuzzle{
                 printShape();
                 System.out.println("Please enter # to finalize your shape or enter coordinates "+ (shape.size() + 1) +" in x y format");
                 String input = scanner.next();
+                input += scanner.nextLine();
                 if (input.equals("#")) {
                     System.out.println("Your finalized shape is");
                     printShape();
                     System.out.println("Please key in test coordinates in x y format or enter # to quit the game");
                     String input2 = scanner.next();
-
+                    input2 += scanner.nextLine();
                     if (input2.equals("#")) {
                         System.out.println("Thank you for playing the GIC geometry puzzle app");
                         System.out.println("Have a nice day!");
-                       System.exit(130);
+                       System.exit(0);
                     }
                     else{
                         printShape();
-                        System.out.println("Please enter coordinates " + (shape.size() + 1) + " in x y format");
-                        addCoordinate(scanner);
+                        String[] coordinate = input.split(" ");
+                        x = Integer.parseInt(coordinate[0]);
+                        y = Integer.parseInt(coordinate[1]);
+                        addCoordinate(scanner,x,y);
                     }
                 }
                 else{
-                    addCoordinate(scanner);
+                    String[] coordinate = input.split(" ");
+                    x = Integer.parseInt(coordinate[0]);
+                    y = Integer.parseInt(coordinate[1]);
+                    addCoordinate(scanner,x,y);
                 }
             }
-
             else{
-                System.out.println("Your current shape is incomplete");
-                printShape();
-                System.out.println("Please enter coordinates " + (shape.size() + 1) + " in x y format");
-                addCoordinate(scanner);
+                if(shape.size() > 3) {
+                    System.out.println("New coordinates" + "(" + point.get(0) + "," + point.get(1) + ")" + " is invalid!!!");
+                    shape.remove(point);
+                    System.out.println("Your current shape is ");
+                    printShape();
+                }else{
+                    System.out.println("Your current shape is incomplete");
+                }
+                System.out.println("Please enter coordinates " + (shape.size() + 1)  + " in x y format");
+                x = scanner.nextInt();
+                y = scanner.nextInt();
+                addCoordinate(scanner,x,y);
             }
 
         } else {
-            System.out.println("New coordinates" + "("+point.get(0)+","+point.get(1)+")" + " is invalid!!!");
-            if(!shape.contains(point))
-                shape.add(point);
-            else
-                System.out.println("Not adding new coordinates to the current shape.");
-            System.out.println("Your current shape is incomplete");
-            printShape();
+            if(shape.size() > 3) {
+                System.out.println("New coordinates" + "(" + point.get(0) + "," + point.get(1) + ")" + " is invalid!!!");
+                System.out.println("Not Adding new coordinates to current shape");
+                shape.remove(point);
+                System.out.println("Your current shape is ");
+                printShape();
+            }else{
+                System.out.println("Your current shape is incomplete");
+            }
             System.out.println("Please enter coordinates " + (shape.size() + 1)  + " in x y format");
-            addCoordinate(scanner);
+            x = scanner.nextInt();
+            y = scanner.nextInt();
+            addCoordinate(scanner,x,y);
         }
     }
 
-    public static boolean isValidCoordinate(List<Integer> point) {
-        return !shape.contains(point) && !(shape.size() < 3);
-    }
-
-    public static void generateRandomShape(Scanner scanner) {
-        Random random = new Random();
-        Integer numOfPoints = random.nextInt(6) + 3; // Random between 3 to 8 points
-        shape = new ArrayList<>();
-        List<Integer> point = new ArrayList<>();
-        for (Integer i = 0; i < numOfPoints; i++) {
-            point.add(random.nextInt(10));
-            point.add(random.nextInt(10));
+    private static boolean isValidCoordinate(List<Integer> point) {
+        if(!shape.contains(point)) {
             shape.add(point);
+            return !(shape.size() < 3);
+        }
+        else {
+            return !shape.contains(point) && !(shape.size() < 3);
+        }
+    }
+
+    public void generateRandomShape(Scanner scanner) {
+
+        Random random = new Random(System.currentTimeMillis());
+        Random random2 = new Random(System.nanoTime());
+        Integer sides = random.nextInt(6) + 3; // Random between 3 to 8 points
+        shape = new ArrayList<>();
+
+        Integer[][] pointArr = new Integer[sides][2];
+
+        for (int i = 0; i < sides; i++) {
+            pointArr[i][0]= random.nextInt(10);
         }
 
-        System.out.println("Your random shape is");
-        printShape();
-        System.out.println("Please key in test coordinates in x y format or enter # to quit the game");
-//        if (input.equals("#")) {
-//            System.out.println("Thank you for playing the GIC geometry puzzle app");
-//            System.out.println("Have a nice day!");
-//            return;
-//        }
-        addCoordinate(scanner);
+        for (int i = 0; i < sides; i++) {
+            pointArr[i][1]= random2.nextInt(10);
+        }
+
+        for(int i =0; i<pointArr.length;i++){
+            shape.add(Arrays.asList(pointArr[i]));
+        }
+        if(!ConvexShapeValidator.isConvexShape(shape)){
+            generateRandomShape(scanner);
+        }else {
+
+            System.out.println("Your random shape is");
+            printShape();
+            puzzleSection(scanner);
+        }
     }
 
     private static void printShape() {
@@ -98,26 +131,26 @@ public class GeometryPuzzle{
                 .forEach(System.out::println);
     }
 
-    public static void puzzleSection(Scanner scanner) {
+    public void puzzleSection(Scanner scanner) {
         System.out.println("Please key in test coordinates in x y format or enter # to quit the game");
         String input = scanner.next();
-
+        input += scanner.nextLine();
         if (input.equals("#")) {
             System.out.println("Thank you for playing the GIC geometry puzzle app");
             System.out.println("Have a nice day!");
-            return;
+            System.exit(0);
         }
-        Integer x = scanner.nextInt();
-        Integer y = scanner.nextInt();
+        String[] coordinate = input.split(" ");
+        int x = Integer.parseInt(coordinate[0]);
+        int y = Integer.parseInt(coordinate[1]);
         Integer[] testPoint = new Integer[2];
         testPoint[0] =x;
         testPoint[1]=y;
         if (isPointInsideShape(testPoint)) {
-            System.out.println("Coordinates " +  "("+testPoint[0]+","+testPoint[1]+")"+ " is within your finalized shape");
+            System.out.println("Coordinates " +  "("+testPoint[0]+","+testPoint[1]+")"+ " is inside your finalized shape");
         } else {
             System.out.println("Sorry, coordinates " +  "("+testPoint[0]+","+testPoint[1]+")"+ " is outside of your finalized shape");
         }
-
         puzzleSection(scanner);
     }
 
@@ -133,7 +166,6 @@ public class GeometryPuzzle{
             }
             j = i;
         }
-
         return result;
     }
 }
--- src/main/java/com/gic/geopuzzle/GeopuzzleApplication.java
+++ src/main/java/com/gic/geopuzzle/GeopuzzleApplication.java
@@ -1,41 +1,42 @@
 package com.gic.geopuzzle;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 @SpringBootApplication
 public class GeopuzzleApplication {
 
 	public static void main(String[] args) {
+		try {
+			GeometryPuzzle ge = new GeometryPuzzle();
 
+			Scanner scanner = new Scanner(System.in);
 
+			System.out.println("Welcome to the GIC geometry puzzle app");
 
+			System.out.println("[1] Create a custom shape");
+			System.out.println("[2] Generate a random shape");
 
+			System.out.print("Enter your choice: ");
 
+			int choice = scanner.nextInt();
 
+			if (choice == 1) {
+				ge.createCustomShape(scanner);
+			} else if (choice == 2) {
+				ge.generateRandomShape(scanner);
+			}
 
-
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Welcome to the GIC geometry puzzle app");
-
-		System.out.println("[1] Create a custom shape");
-		System.out.println("[2] Generate a random shape");
-		System.out.print("Enter your choice: ");
-		int choice = scanner.nextInt();
-
-		if (choice == 1) {
-			GeometryPuzzle.createCustomShape(scanner);
-		} else if (choice == 2) {
-			GeometryPuzzle.generateRandomShape(scanner);
+			ge.puzzleSection(scanner);
+			System.out.println("Thank you for playing the GIC geometry puzzle app");
+			System.out.println("Have a nice day!");
 		}
-
-		GeometryPuzzle.puzzleSection(scanner);
-		System.out.println("Thank you for playing the GIC geometry puzzle app");
-		System.out.println("Have a nice day!");
+		catch(Exception e){
+			System.out.println("Application Crashed...!  "+ e.getMessage());
+			main(new String[]{"", ""});
+		}
 	}
+
 
 }
